72e7d7f10f6fbead41fdcd7e9ca2d6b5b5df8724
Mike Andrew <mjandrew3@users.noreply.github.com>
2025-05-14 00:41:16 +0000

    Pause and restart with formatting

--- daemon.go
+++ daemon.go
@@ -294,7 +294,7 @@ func (daemon *Daemon) CancelHandler(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
-// PauseHandler pauses job execution
+// CancelHandler cancels job execution
 func (daemon *Daemon) PauseHandler(w http.ResponseWriter, _ *http.Request) {
 	log.Infof("Received pause handler request")
 	// Add headers to allow AJAX
